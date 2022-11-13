574fdbf79444c544d6b9cf6dd491e6dbf891dead
Tin Kanjovsky <tkanjovsky@student.unipu.hr>
2022-11-13 15:21:35 +0100

    dodajem post

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import cors from "cors";
 const app = express(); // instanciranje aplikacije
 const port = 3000; // port na kojem će web server slušati
 
-app.use(cors());
+app.use(cors(), express.json());
 
 app.get("/posts", (req, res) => {
   let posts = storage.posts;
@@ -13,9 +13,20 @@ app.get("/posts", (req, res) => {
 
   if (query.city) {
     console.log(`Treba pretražiti po 'gradu' = ${query.city}`);
-    posts = posts.filter((x) => x.city == query.city);
+    posts = posts.filter(
+      (x) => x.city.toLocaleLowerCase() == query.city.toLocaleLowerCase()
+    );
   }
 
   res.json(posts);
+});
+
+app.post("/posts", (req, res) => {
+  let data = req.body;
+  data.id = 1 + storage.posts.reduce((max, el) => Math.max(el.id, max), 0);
+  console.log(data);
+  storage.posts.push(data);
+
+  res.json(data);
 });
 app.listen(port, () => console.log(`Slušam na portu ${port}!`));
