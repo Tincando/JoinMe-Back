1ab8c27c1747bf3dbf2c56559aa638f7dc6722cd
Tin Kanjovsky <tkanjovsky@student.unipu.hr>
2022-11-20 19:31:41 +0100

    povezujem se sa  mongodb

--- src/index.js
+++ src/index.js
@@ -2,12 +2,37 @@ import express from "express";
 import storage from "./memory_storage";
 import cors from "cors";
 
+import connect from "./db.js";
+
 const app = express(); // instanciranje aplikacije
 const port = 3000; // port na kojem će web server slušati
 
 app.use(cors(), express.json());
 
-app.get("/posts", (req, res) => {
+app.get("/posts", async (req, res) => {
+  let query = req.query;
+  let filter = {};
+  if (query.title) {
+    filter["title"] = new RegExp(query.title);
+  }
+  console.log("Filter za Mongo", filter);
+
+  let db = await connect();
+  let cursor = await db
+    .collection("events")
+    .find(filter)
+    .sort({ postedAt: -1 });
+  let results = await cursor.toArray();
+
+  // Premještanje atributa _id u id
+  results.forEach((e) => {
+    e.id = e._id;
+    delete e._id;
+  });
+  res.json(results);
+});
+
+app.get("/posts_memory", (req, res) => {
   let posts = storage.posts;
   let query = req.query;
 
